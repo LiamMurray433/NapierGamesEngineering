e669c9d46913b034970819d1dcaf75cf24c6e300
Liam Murray <l.murray8391@gmail.com>
2025-10-08 19:10:47 +0100

    add object update and render functions

--- SpaceInvaders/game_system.cpp
+++ SpaceInvaders/game_system.cpp
@@ -1,6 +1,20 @@
 #include "game_system.hpp"
 
+using param = Parameters;
+
 void init() {
 	std::shared_ptr<Invader> inv = std::make_shared<Invader>(sf::IntRect(sf::Vector2i(0, 0), sf::Vector2i(32, 32)), { 100,100 });
 	ships.push_back(inv);
+}
+
+void update(const float& dt) {
+	for (std::shared_ptr<Ship>& s) {
+		s->update(dt);
+	}
+}
+
+void render(sf::RenderWindow& window) {
+	for (const std::shared_ptr<Ship>& s : ships) {
+		window.draw(*(s.get()));
+	}
 }
