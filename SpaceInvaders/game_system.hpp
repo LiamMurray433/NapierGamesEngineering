83e3a5698e7e5ee9b1d047fba329fe021ebd92fd
Liam Murray <l.murray8391@gmail.com>
2025-10-12 11:24:41 +0100

    added more functions

--- SpaceInvaders/game_system.hpp
+++ SpaceInvaders/game_system.hpp
@@ -2,10 +2,16 @@
 #include <vector>
 #include <memory>
 #include "ship.hpp"
+#include <SFML/Graphics.hpp>
+
 struct GameSystem {
+
+	static std::vector<std::shared_ptr<Ship>> ships;
+	static sf::Texture spritesheet;
 	static void init();
 	static void clean();
+	static void reset();
 	static void update(const float& dt);
 	static void render(sf::RenderWindow& window);
-	static std::vector<std::shared_ptr<ship>> ships; //vector of shared pointers
+	
 }
