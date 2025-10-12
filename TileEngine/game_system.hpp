887a10dabe47e1bdadcf50f7097f6912dba71d5d
Liam Murray <l.murray8391@gmail.com>
2025-10-12 18:36:21 +0100

    refactor to handle scene management

--- TileEngine/game_system.hpp
+++ TileEngine/game_system.hpp
@@ -1,17 +1,22 @@
 //game_system.hpp
 #include <vector>
 #include <memory>
-#include "ship.hpp"
 #include <SFML/Graphics.hpp>
 
-struct GameSystem {
+class Scene;
 
-	static std::vector<std::shared_ptr<Ship>> ships;
-	static sf::Texture spritesheet;
-	static void init();
+class GameSystem {
+public:
+	static void start(unsigned int width, unsigned int height,
+		const std::string& name);
+
 	static void clean();
 	static void reset();
-	static void update(const float& dt);
-	static void render(sf::RenderWindow& window);
+	static void set_active_scene(const std::shared_ptr<Scene>& act_sc);
 
+private:
+	static void init();
+	static void update(const float &dt);
+	static void _render(sf::RenderWindow& window);
+	static std::shared_ptr<Scene> _active_scene;
 };
