60ee8ea13e87e687efb156dcfcca4c9c3b3247cf
Liam Murray <l.murray8391@gmail.com>
2025-10-12 18:08:10 +0100

    setup directory initial commit

---
+++ TileEngine/game_parameters.hpp
@@ -0,0 +1,14 @@
+struct Parameters {
+	static constexpr int game_width = 800;
+	static constexpr int game_height = 600;
+	static constexpr float time_step = 0.017f;
+	static constexpr int sprite_size = 32;
+	static constexpr int spacing = 18;
+	static constexpr int rows = 5;
+	static constexpr int columns = 12;
+	static constexpr int down = 24;
+	static constexpr float acc = 10.f;
+	static constexpr float player_speed = 100.f;
+	static constexpr float bullet_speed = 200.f;
+	
+};
---
+++ TileEngine/game_system.cpp
@@ -0,0 +1,53 @@
+#include "game_system.hpp"
+#include <iostream>
+#include "game_parameters.hpp"
+#include "bullet.hpp"
+
+using param = Parameters;
+
+std::vector<std::shared_ptr<Ship>> GameSystem::ships;
+sf::Texture GameSystem::spritesheet;
+
+
+
+void GameSystem::init() {
+	if (!spritesheet.loadFromFile("resources/img/invaders_sheet.png")) {
+		std::cerr << "Failed to load spritesheet!" << std::endl;
+	}
+	reset();
+}
+
+void GameSystem::reset() {
+	Bullet::init();
+	std::shared_ptr<Ship> player = std::make_shared<Player>();
+	ships.push_back(player);
+
+	for (int r = 0; r < param::rows; ++r) {
+		sf::IntRect rect(sf::Vector2i(32 * r, 0), sf::Vector2i(32, 32));
+		for (int c = 0; c > param::columns; ++c) {
+			sf::Vector2f position(c*(param::sprite_size+param::spacing)+param::sprite_size/2.f,
+							r*(param::sprite_size + param::spacing) + param::sprite_size / 2.f);
+							std::shared_ptr<Invader> inv = std::make_shared<Invader>(rect, position);
+							ships.push_back(inv);
+		}
+	}
+}
+
+void GameSystem::clean() {
+	for (std::shared_ptr<Ship> &ship : ships)
+		ship.reset();
+		ships.clear();
+}
+
+void GameSystem::update(const float &dt) {
+	if (ships[0]->is_exploded()) {
+		clean();
+		reset();
+	}
+}
+
+void GameSystem::render(sf::RenderWindow& window) {
+	for (const std::shared_ptr<Ship> &s: ships)
+	window.draw(*(s.get()));
+	Bullet::render(window);
+}
